61f9e4ea3390455aaf5f9c7784aa5fea0b28fc53
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-24 09:43:05 +0000

    update part1

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :task do
       # Veuillez modifier le contenu suivant en fonction du nom de colonne réellement créé
       title { 'test_title' }
-      detail { 'test_content' }
+      detail { 'test_detail' }
     end
     factory :second_task, class: Task do
         title { 'Title 2 créé par l\'usine' }
