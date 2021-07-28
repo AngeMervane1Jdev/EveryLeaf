afecc7de2bd4f281e87a3cd3c4e86d7832a5efc0
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-28 06:28:13 +0000

    step5

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
     has_many :tasks, dependent: :destroy
+    has_many :labels, dependent: :destroy
 
     private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name:"Admin",email:'myadmin@gmail.com',password:'adminpword0',admin:true)
-@taskss=Task.where(user_id:nil)
-@taskss.each do |e|
-e.update(user_id:1)
+
+# User.create(name:"Admin",email:'myadmin@gmail.com',password:'adminpword0',admin:true)
+# @taskss=Task.where(user_id:nil)
+# @taskss.each do |e|
+# e.update(user_id:1)
+#end
+status = ['unstarted', 'progress', 'completed']
+priority = ['Low', 'Medium', 'High']
+
+20.times do |i|
+    User.create(
+        name: "fake#{i}name",
+        email: "fakeemail#{i}@gmail.com",
+        password: "password"
+    )
+end
+
+21.times do |n|
+    Label.create(name: "tag_#{n}_#{n+12}")
+end
+
+
+100.times do |i|
+    Task.create(
+      title: "task#{i + 1}",
+      detail: "task detail#{i + 1}",
+      deadline: DateTime.now + 10,
+      status: status[rand(3)],
+      priority: priority[rand(3)],
+      user_id: rand(1..21)
+    )
+end
+
+20.times do |n|
+    Label.create(
+        name: "tag_#{n}_#{n-12}"
+    )
+end
+
+100.times do |i|
+    Labeling.create!(
+      task_id: rand(1..100),
+      label_id: rand(1..5)
+    )
 end
